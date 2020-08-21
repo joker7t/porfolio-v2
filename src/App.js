01084d56c6b6a498d2247c12745dde5f507412a0
joker7t <batoan131196@gmail.com>
2020-08-21 21:22:02 +0700

    add basic route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useRef, useEffect } from 'react';
-import './App.scss';
+import style from './App.module.scss';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './components/home/Home';
+import About from './components/about/About';
+import Skill from './components/skill/Skill';
+import Work from './components/work/Work';
+import Contact from './components/contact/Contact';
 
 function App() {
   const cursorRef = useRef(null);
@@ -19,13 +23,17 @@ function App() {
 
   return (
     <Router>
-      <div style={{ cursor: 'none' }}>
+      <div className={style.App}>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/skill' component={Skill} />
+          <Route exact path='/work' component={Work} />
+          <Route exact path='/contact' component={Contact} />
           <Redirect from="*" to="/" />
         </Switch>
       </div>
-      <div className='cursor' ref={cursorRef}></div>
+      <div className={style.Cursor} ref={cursorRef}></div>
     </Router>
   );
 }
