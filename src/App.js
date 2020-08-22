4a5ff5e448b4f3d52b9dfed709a34463ca4a5686
joker7t <batoan131196@gmail.com>
2020-08-22 18:26:24 +0700

    add new loading page

--- src/App.js
+++ src/App.js
@@ -34,23 +34,36 @@ function App() {
 	// <Loading page={appRef} setIsLoading={setIsLoading} setIsLoaded={setIsLoaded} />
 
 	return (
-		<div className={`${style.App} ${isLoading ? 'loading' : ''} ${isLoaded ? 'loaded' : ''}`} ref={appRef}>
-			{isLoading ? (
-				<LoadingPage />
-			) : (
-				<Router>
-					<NavigationBar />
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/about" component={About} />
-						<Route exact path="/skill" component={Skill} />
-						<Route exact path="/work" component={Work} />
-						<Route exact path="/contact" component={Contact} />
-						<Redirect from="*" to="/" />
-					</Switch>
-				</Router>
-			)}
-			<div className={style.Cursor} ref={cursorRef}></div>
+		<div>
+			<div class="splash">
+				<div class="splash_logo">BJL</div>
+				<div class="splash_svg">
+					<svg width="100%" height="100%">
+						<rect width="100%" height="100%" />
+					</svg>
+				</div>
+				<div class="splash_minimize">
+					<svg width="100%" height="100%">
+						<rect width="100%" height="100%" />
+					</svg>
+				</div>
+			</div>
+			<div class="text">
+				<div className={`${style.App} ${isLoading ? 'loading' : ''} ${isLoaded ? 'loaded' : ''}`} ref={appRef}>
+					<Router>
+						<NavigationBar />
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/about" component={About} />
+							<Route exact path="/skill" component={Skill} />
+							<Route exact path="/work" component={Work} />
+							<Route exact path="/contact" component={Contact} />
+							<Redirect from="*" to="/" />
+						</Switch>
+					</Router>
+					<div className={style.Cursor} ref={cursorRef}></div>
+				</div>
+			</div>
 		</div>
 	);
 }
