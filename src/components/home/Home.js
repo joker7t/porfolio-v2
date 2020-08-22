cd5cd90c1838242155f7f154925351de47204fab
joker7t <batoan131196@gmail.com>
2020-08-22 14:31:51 +0700

    add name card

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import style from './scss/Home.module.scss';
+import { Row, Col } from 'react-bootstrap';
+import NameCard from './NameCard';
 
 const Home = () => {
-    return (
-        <div className={style.Home}>
-            Home
-        </div>
-    );
-}
+	return (
+		<div className={style.Home}>
+			<Row style={{ height: '100%' }}>
+				<Col></Col>
+				<Col>
+					<NameCard />
+				</Col>
+			</Row>
+		</div>
+	);
+};
 
 export default Home;
---
+++ src/components/home/NameCard.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import style from './scss/NameCard.module.scss';
+
+const NameCard = () => {
+	const [isAnimate, setIsAnimate] = useState(false);
+
+	useEffect(() => {
+		function animate() {
+			setIsAnimate(true);
+		}
+		animate();
+	});
+
+	return (
+		<div>
+			<div className={`${isAnimate ? style.Animate : ''}`}>
+				<div className={style.Unbreakable}>
+					<span className={style.Line1}>I'M</span>
+					<span className={style.LineAdditional}>A</span>
+					<span className={style.Line2}>WEB</span>
+					<span className={style.Line3}>DEVELOPER</span>
+				</div>
+				<div className={style.Name}>TOAN NGUYEN</div>
+			</div>
+		</div>
+	);
+};
+
+export default NameCard;
