38db107a8eabfce6ddc396bd6d3e0d46fc5f4159
joker7t <batoan131196@gmail.com>
2020-08-22 10:49:05 +0700

    add neon button

--- src/util/components/NavigationBar.js
+++ src/util/components/NavigationBar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import style from './scss/NavigationBar.module.scss';
+import { TimelineMax } from 'gsap';
 
 const NavigationBar = () => {
+	const line1Ref = useRef(null);
+	const [isPowerActive, setIsPowerActive] = useState(false);
+
 	const data = [
 		{
 			url: 'aa',
@@ -17,6 +21,15 @@ const NavigationBar = () => {
 		},
 	];
 
+	useEffect(() => {
+		const tl = new TimelineMax();
+		// tl.to(line1Ref.current, 1, { left: '100%' });
+
+		// setTimeout(() => {
+		// 	setIsPowerActive(true);
+		// }, 1000);
+	}, []);
+
 	const showItems = () =>
 		data.map((item, i) => (
 			<li>
@@ -30,7 +43,13 @@ const NavigationBar = () => {
 		<div className={style.NavigationBar}>
 			<ul className={style.Container}>
 				<li className={style.PowerButton}>
-					<i class="icon-reorder"></i> nguon
+					<div className={`${style.Power} ${isPowerActive ? style.Active : ''}`}>
+						<span ref={line1Ref}></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						Neon button
+					</div>
 				</li>
 				{showItems()}
 				<li className={style.Black}></li>
