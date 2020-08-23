47d1bd4ff9e06047e1f9c5ece80777a8388a2e11
joker7t <batoan131196@gmail.com>
2020-08-23 08:59:07 +0700

    add loading again

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,10 +3,12 @@ import style from './scss/Home.module.scss';
 import { Row, Col } from 'react-bootstrap';
 import NameCard from './NameCard';
 import Intro from './Intro';
+import Loading from '../loading/Loading';
 
 const Home = () => {
 	return (
 		<div className={style.Home}>
+			<Loading />
 			<Row style={{ height: '100%' }}>
 				<Col>
 					<Intro />
