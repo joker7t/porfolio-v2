8237262d109b3494deacd0c27b88b06ed1a5f41e
joker7t <batoan131196@gmail.com>
2020-08-24 23:37:00 +0700

    add title for about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,28 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import style from './scss/About.module.scss';
 import './test.scss';
 
 const About = () => {
 	return (
 		<div className={style.AboutContainer}>
-			<div className={style.Wrapper}>
-				<div className={style.Text}>ABOUT ME</div>
+			<div class="wrapper">
+				<div class="clip-text clip-text_one">JINTOS</div>
+				<div class="clip-text clip-text_fifteen clip-text--no-textzone">JINTOS</div>
+				<div class="clip-text clip-text_twelve clip-text--cover">JINTOS</div>
+				<div class="clip-text clip-text_tree clip-text--no-textzone">JINTOS</div>
+				<div class="clip-text clip-text_two">JINTOS</div>
+				<div class="clip-text clip-text_fourteen clip-text--cover">JINTOS</div>
+				<div class="clip-text clip-text_tree">JINTOS</div>
+				<div class="clip-text clip-text_eleven clip-text--cover">JINTOS</div>
+				<div class="clip-text clip-text_four">JINTOS</div>
+				<div class="clip-text clip-text_five">JINTOS</div>
+				<div class="clip-text clip-text_six">JINTOS</div>
+				<div class="clip-text clip-text_seven">JINTOS</div>
+				<div class="clip-text clip-text_eight">JINTOS</div>
+
+				<div class="clip-text clip-text_nine">JINTOS</div>
+				<div class="clip-text clip-text_ten">JINTOS</div>
+				<div class="clip-text clip-text_thirteen clip-text--cover">JINTOS</div>
 			</div>
 		</div>
 	);
