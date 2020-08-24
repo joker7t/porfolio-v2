5ba7ada3bf3c48384a9b28eb6c42115b8a24554b
joker7t <batoan131196@gmail.com>
2020-08-24 23:12:14 +0700

    add title for about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,13 @@ import style from './scss/About.module.scss';
 import './test.scss';
 
 const About = () => {
-	return <div className={style.AboutContainer}></div>;
+	return (
+		<div className={style.AboutContainer}>
+			<div className={style.Wrapper}>
+				<div className={style.Text}>ABOUT ME</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
