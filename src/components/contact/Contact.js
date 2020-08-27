f9562140044c6cb369d3272d5d5e7a96b0b5f6f4
joker7t <batoan131196@gmail.com>
2020-08-27 23:08:57 +0700

    spilt contact file

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,121 +1,13 @@
 import React, { useEffect } from 'react';
 import style from './scss/Contact.module.scss';
+import Intro from './Intro';
 // import ContactPicture from './ContactPicture';
 // <ContactPicture />
 
 const Contact = () => {
-	useEffect(() => {
-		// 4 states per letter: Transparent | Line | Block | Visible.
-		function decodeText() {
-			const text = document.getElementsByClassName(style.DecodeText)[0];
-			// debug with
-			// console.log(text);
-			// console.log(text.children.length);
-			// assign the placeholder array its places
-			if (text) {
-				const state = [];
-				for (let i = 0, j = text.children.length; i < j; i++) {
-					text.children[i].classList.remove(style.State1, style.State2, style.State3);
-					state[i] = i;
-				}
-
-				// shuffle the array to get new sequences each time
-				const shuffled = shuffle(state);
-				for (let i = 0, j = shuffled.length; i < j; i++) {
-					const child = text.children[shuffled[i]];
-					const classes = child.classList;
-					// fire the first one at random times
-					const state1Time = Math.round(Math.random() * (2000 - 300)) + 50;
-					if (classes.contains(style.TextAnimation)) {
-						setTimeout(firstStages.bind(null, child), state1Time);
-					}
-				}
-			}
-		}
-
-		// send the node for later .state changes
-		function firstStages(child) {
-			if (child.classList.contains(style.State2)) {
-				child.classList.add(style.State3);
-			} else if (child.classList.contains(style.State1)) {
-				child.classList.add(style.State2);
-			} else if (!child.classList.contains(style.State1)) {
-				child.classList.add(style.State1);
-				setTimeout(secondStages.bind(null, child), 100);
-			}
-		}
-		function secondStages(child) {
-			if (child.classList.contains(style.State1)) {
-				child.classList.add(style.State2);
-				setTimeout(thirdStages.bind(null, child), 100);
-			} else if (!child.classList.contains(style.State1)) {
-				child.classList.add(style.State1);
-			}
-		}
-		function thirdStages(child) {
-			if (child.classList.contains(style.State2)) {
-				child.classList.add(style.State3);
-			}
-		}
-
-		function shuffle(array) {
-			var currentIndex = array.length,
-				temporaryValue,
-				randomIndex;
-
-			// While there remain elements to shuffle...
-			while (0 !== currentIndex) {
-				// Pick a remaining element...
-				randomIndex = Math.floor(Math.random() * currentIndex);
-				currentIndex -= 1;
-				// And swap it with the current element.
-				temporaryValue = array[currentIndex];
-				array[currentIndex] = array[randomIndex];
-				array[randomIndex] = temporaryValue;
-			}
-			return array;
-		}
-
-		decodeText();
-
-		setInterval(function () {
-			decodeText();
-		}, 10000);
-
-		//eslint-disable-next-line
-	}, []);
-
 	return (
 		<div className={style.ContactContainer}>
-			<div className={style.DecodeText}>
-				<div className={style.TextAnimation}>l</div>
-				<div className={style.TextAnimation}>e</div>
-				<div className={style.TextAnimation}>t</div>
-				<div className={style.TextAnimation}>'</div>
-				<div className={style.TextAnimation}>s</div>
-				<div className={style.Space}></div>
-				<div className={style.TextAnimation}>d</div>
-				<div className={style.TextAnimation}>o</div>
-				<div className={style.Space}></div>
-				<div className={style.TextAnimation}>s</div>
-				<div className={style.TextAnimation}>o</div>
-				<div className={style.TextAnimation}>m</div>
-				<div className={style.TextAnimation}>e</div>
-				<div className={style.TextAnimation}>t</div>
-				<div className={style.TextAnimation}>h</div>
-				<div className={style.TextAnimation}>i</div>
-				<div className={style.TextAnimation}>n</div>
-				<div className={style.TextAnimation}>g</div>
-				<div className={style.Space}></div>
-				<div className={style.TextAnimation}>t</div>
-				<div className={style.TextAnimation}>o</div>
-				<div className={style.TextAnimation}>g</div>
-				<div className={style.TextAnimation}>e</div>
-				<div className={style.TextAnimation}>t</div>
-				<div className={style.TextAnimation}>h</div>
-				<div className={style.TextAnimation}>e</div>
-				<div className={style.TextAnimation}>r</div>
-			</div>
+			<Intro />
 		</div>
 	);
 };
